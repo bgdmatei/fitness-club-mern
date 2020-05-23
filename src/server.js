9ed537873d0dd0adcf0ff8f03336763bb8f0f34d
bogdanmatei <matei90bogdan@yahoo.com>
2020-05-24 00:27:10 +0100

    added gitignore

--- src/server.js
+++ src/server.js
@@ -1,14 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-const UserController = require("./controllers/UserController");
 
 const PORT = process.env.PORT || 8000;
-
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 
 app.use(cors());
 app.use(express.json());
@@ -16,8 +10,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello from express");
 });
-
-app.post("/register", UserController.store);
 
 try {
   mongoose.connect(process.env.MONGO_DB_CONNECTION, {
