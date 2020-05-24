44aae44a6bd115c91e4d27b3a0d08c95c75c1e1e
bogdanmatei <matei90bogdan@yahoo.com>
2020-05-24 16:07:02 +0100

    mongodb setup

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 app.use(cors());
 app.use(express.json());
