28915b3530afbc2a673f53bb79ae9ff850dc3cf4
bogdanmatei <matei90bogdan@yahoo.com>
2020-05-24 17:04:32 +0100

    fix

---
+++ src/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
+
+app.post("/register", UserController.store);
+
+try {
+  mongoose.connect(process.env.MONGO_DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("MongoDB connected");
+} catch (error) {
+  console.log(error);
+}
+
+app.listen(PORT, () => {
+  console.log(`listening to ${PORT}`);
+});
