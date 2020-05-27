740f6455089ccdd6905646ab5636d7a2baab7223
bogdanmatei <matei90bogdan@yahoo.com>
2020-05-27 23:51:04 +0100

    created pages and set routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Routes from "./routes";
 import "./App.css";
 
 function App() {
-  return <div className="App">Learning the MERN stack</div>;
+  return (
+    <Container>
+      <h1>Fitness app</h1>
+      <Routes />
+    </Container>
+  );
 }
 
 export default App;
