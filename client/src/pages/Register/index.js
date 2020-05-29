f47c046d8dc26f2d912a146d7574b5891dabe122
bogdanmatei <matei90bogdan@yahoo.com>
2020-05-29 19:09:42 +0100

    created register page

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -2,15 +2,22 @@ import React, { useState } from "react";
 import api from "../../services/api";
 import { Button, Form, FormGroup, Input, Container } from "reactstrap";
 
-export default function Login({ history }) {
+export default function Register({ history }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   const handleSubmit = async (evt) => {
     evt.preventDefault();
     console.log("result of the submit", email, password);
 
-    const response = await api.post("/login", { email, password });
+    const response = await api.post("/user/register", {
+      email,
+      password,
+      firstName,
+      lastName,
+    });
     const userId = response.data._id || false;
 
     if (userId) {
@@ -23,8 +30,26 @@ export default function Login({ history }) {
 
   return (
     <Container>
-      <h2>Login</h2>
+      <h2>Register</h2>
       <Form onSubmit={handleSubmit}>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Input
+            type="text"
+            name="secondName"
+            id="secondName"
+            placeholder="Your last name"
+            onChange={(evt) => setLastName(evt.target.value)}
+          />
+        </FormGroup>
+        <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+          <Input
+            type="text"
+            name="firstName"
+            id="firstName"
+            placeholder="Your first name"
+            onChange={(evt) => setFirstName(evt.target.value)}
+          />
+        </FormGroup>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Input
             type="email"
