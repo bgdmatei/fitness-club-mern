37d467dcd3ef82c9e37069fd0594a031954a9455
bogdanmatei <matei90bogdan@yahoo.com>
2020-06-28 17:58:33 +0100

    Populated my events

--- client/src/pages/MyRegistrations/index.js
+++ client/src/pages/MyRegistrations/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../../services/api";
+import moment from "moment";
 
 export default function MyRegistrations() {
   const [myEvents, setMyEvents] = useState([]);
@@ -7,14 +8,29 @@ export default function MyRegistrations() {
 
   useEffect(() => {
     getMyEvents();
-  });
+  }, []);
 
   const getMyEvents = async () => {
     try {
-      const response = await api.get("registration", { headers: { user } });
+      const response = await api.get("/registration", { headers: { user } });
       console.log(response.data);
+      setMyEvents(response.data);
     } catch (error) {}
   };
 
-  return <div> My Registrations component</div>;
+  return (
+    <ul className="events">
+      {myEvents.map((event) => (
+        <li key={event._id}>
+          <div>{event.eventTitle}</div>
+          <div className="event-details">
+            <span>Event date: {moment(event.eventDate).format("l")}</span>
+            <span>Event price: £{parseFloat(event.eventPrice).toFixed(2)}</span>
+            <span>User email: {event.userEmail}</span>
+            <span>Status: {event.approved}</span>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
 }
